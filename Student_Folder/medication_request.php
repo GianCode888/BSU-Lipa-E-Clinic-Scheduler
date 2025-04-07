fe7a09f8600464745aaaa4f54c0c776ba365f2a0
GianCode888 <liberakylegian35@gmail.com>
2025-04-07 14:28:57 +0800

    fix

--- Student_Folder/medication_request.php
+++ Student_Folder/medication_request.php
@@ -2,16 +2,21 @@
 session_start();
 include '../eclinic_database.php';
 
+$database = new DatabaseConnection();     
+$conn = $database->getConnect();
+
 $student_id = $_SESSION['user_id'];
 $request_details = $_POST['medication'];
 
 $sql = "INSERT INTO medication_requests (student_id, medication) VALUES (?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("is", $student_id, $request_details);
+$stmt->bindparam(1, $student_id);
+$stmt->bindparam(2, $request_details);
+
 
 if ($stmt->execute()) {
     echo "Medication request submitted successfully.";
 } else {
-    echo "Error: " . $conn->error;
+    echo "Error: " . $conn->errorInfo()[2];
 }
 ?>
